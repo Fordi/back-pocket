b2a95910d7f76c8b52774984be8ffd99046d9ae6
Bryan Elliott <bryan.elliott@cesium.com>
2025-08-25 14:35:29 -0400

    Update email regexp

--- src/email-regexp.js
+++ src/email-regexp.js
@@ -1,25 +1,48 @@
-export const createEmailRegex = (() => {
+// Regular expression that should properly match any email address, and reject anything invalid
+// Must be converted to a RegExp with the `u` flag.
+export const createEmailRegex = ({
+  quotes = true,
+  ips = true,
+  unicode = true,
+} = {}) => {
   // localPart is from from https://www.ietf.org/rfc/rfc822.txt section D
   const localPart = (() => {
-    const char = '[\\x00-\\x7F]';
+    const nonAscii = unicode ? `\u0100-\u{10FFFF}` : "";
+    const char = `[\\x00-\\x7F${nonAscii}]`;
     // char, without 0-31 (CTL), SPACE, (, ), <, >, @, comma, ;, :, \, ", ., [, ] (collectively, SPECIALS)
-    const atom = `[!#-'*+\\-\\/0-9=?A-Z\\^_\`a-z\\{|\\}~]+`;
-    // char, without ", \, \n, but including linearWhiteSpace
-    const linearWhiteSpace = `(?:\r\n[\s\t]*)+`;
-    const qtext = `(?:[\\x00-\\x09\\x0B-\\x20!#-/0-9:-@A-Z\\[\\]-\`a-z{-~\\x7F]|${linearWhiteSpace})`;
-    const quotedPair = `\\\\${char}`;
-    const quotedString = `"(?:${qtext}|${quotedPair})*"`;
-    const word = `(?:${atom}|${quotedString})`;
-    return `${word}(?:\\.${word})*`;
+    const atomChar = `[!#-'*+\\-/0-9=?A-Z\\^_\`a-z\\{|\\}~${nonAscii}]`;
+    const atom = `${atomChar}+`;
+    const linearWhiteSpace = `\\r\\n[\\s\\t]*`;
+    let localPartTerm;
+    if (quotes) {
+      const quotedChar = `[\\x00-\\x09\\x0B-\\x20!#-/0-9:-@A-Z\\[\\]-\`a-z\\{-~\\x7F${nonAscii}]`;
+      const escape = `\\\\`;
+      const quotedEscapeSequence = `${escape}${char}`;
+      const quoted = `"(?:${quotedChar}|${linearWhiteSpace}|${quotedEscapeSequence})*"`;
+      localPartTerm = `(?:${atom}|${quoted})`;
+    } else {
+      localPartTerm = atom;
+    }
+    const maxLen = 64;
+    const maxRx = `(?<!.{${maxLen + 1}})`;
+    return `${localPartTerm}(?:\\.${localPartTerm})*${maxRx}`;
   })();
+
   // RFC822 domains are too-loosely constrained, designed to allow for pre-internet, network-internal "domains".
   // Instead, we'll use internet domain names from https://www.ietf.org/rfc/rfc1035.txt section 2.3.1
   const domain = (() => {
     // Rule slightly altered to allow non-recursive construction, since regexes don't do that.
-    const letDigHyp = `[a-zA-Z0-9\\-]`;
-    const subDomain = `${letDigHyp}+`;
-    return `${subDomain}(?:\.${subDomain})*`;
+    const nameChar = unicode ? `[\\p{L}\\p{N}\\-]` : `[a-zA-Z0-9\\-]`;
+    const subDomain = `${nameChar}+`;
+    const domainName = `${subDomain}(?:\\.${subDomain})*`;
+    if (ips) {
+      const octet = `(?:[0-9]|[0-9]{2}|[01][0-9]{2}|2[0-4][0-9]|25[0-5])`;
+      const ipv6Address = `\\[IP[vV]6(?:(:\\p{Hex_Digit}{0,4}){1,8})\\]`;
+      const ipv4Address = `\\[${octet}(?:\\.${octet}){3}\\]`;
+      return `(?:${domainName}|${ipv6Address}|${ipv4Address})`;
+    }
+    return domainName;
   })();
-  const addrSpec = `${localPart}@${domain}`;
-  return new RegExp(`^${addrSpec}$`);
-});
+  const addrSpec = `(${localPart})@(${domain})`;
+  return new RegExp(`^${addrSpec}$`, "u");
+};
---
+++ src/email-regexp.test.js
@@ -0,0 +1,135 @@
+import { describe, it } from "node:test";
+import { strictEqual } from "node:assert";
+import { createEmailRegex } from "./email-regexp.js";
+
+// Mostly from https://en.wikipedia.org/wiki/Email_address
+const invalidAddresses = [
+  // no @ character
+  `abc.example.com`,
+  // only one @ is allowed outside quotation marks
+  `a@b@c@example.com`,
+  // unterminated quote
+  `a"@example.com`,
+  // none of the special characters in this local-part are allowed outside quotation marks
+  `a(b@example.com`,
+  `a)bl@example.com`,
+  `a,b@example.com`,
+  `a:b@example.com`,
+  `a;b@example.com`,
+  `a<b@example.com`,
+  `a>b@example.com`,
+  `a[b@example.com`,
+  `a]b@example.com`,
+  // quoted strings must be dot separated or be the only element making up the local-part
+  `just"not"right@example.com`,
+  `just."not"right@example.com`,
+  `just"not".right@example.com`,
+  `"not"right@example.com`,
+  `just"wrong"@example.com`,
+  // spaces, quotes, and backslashes may only exist when within quoted strings and preceded by a backslash
+  `not"allowed@example.com`,
+  `not\\allowed@example.com`,
+  `not allowed@example.com`,
+  // even if escaped (preceded by a backslash), spaces, quotes, and backslashes must still be contained by quotes
+  `still"wrong@example.com`,
+  `still wrong@example.com`,
+  `still\\wrong@example.com`,
+  // local-part is longer than 64 characters
+  `1234567890123456789012345678901234567890123456789012345678901234+x@example.com`,
+  // underscore is not allowed in domain part,
+  `i.like.underscores@but_they_are_not_allowed_in_this_part`,
+  // domains may not contain a run of more than one dot
+  `double-dot-not-allowed@in..domain`,
+  // local-part may not contain an unquoted run of more than one dot
+  `double..dot@example.com`,
+];
+
+const validAddresses = [
+  `simple@example.com`,
+  `very.common@example.com`,
+  // case is always ignored after the @ and usually before
+  `FirstName.LastName@EasierReading.org`,
+  // one-letter local-part
+  `x@example.com`,
+  `long.email-address-with-hyphens@and.subdomains.example.com`,
+  // may be routed to user.name@example.com inbox depending on mail server
+  `user.name+tag+sorting@example.com`,
+  // slashes are a printable character, and allowed
+  `name/surname@example.com`,
+  // local domain name with no TLD, although ICANN highly discourages dotless email addresses[32]
+  `admin@example`,
+  // see the List of Internet top-level domains
+  `example@s.example`,
+  // bangified host route used for UUCP mailers
+  `mailhost!username@example.org`,
+  // % escaped mail route to user@example.com via example.org
+  `user%example.com@example.org`,
+  // local-part ending with non-alphanumeric character from the list of allowed printable characters
+  `user-@example.org`,
+];
+const validUnicodeAddresses = [
+  // emoji
+  `I❤️CHOCOLATE@example.com`,
+  // non-roman local-name language examples
+  `名無しの権兵衛@example.co.jp`,
+  `张三@example.co.cn`,
+  `अशोक.कुमार@example.co.in`,
+  `Вася.Пупкин@example.co.ua`,
+  `שראל.ישראל@example.co.il`,
+  `فلان.الفلاني@example.co.ae`,
+  // 移动 is currently a valid TLD
+  `john.doe@yidong.移动`,
+];
+
+const validQuotedAddresses = [
+  // quoted double dot
+  `"john..doe"@example.org`,
+  // bangified host route used for UUCP mailers
+  // include non-letters character AND multiple at sign, the first one being double quoted
+  `"very.(),:;<>[]\\".VERY.\\"very@\\\\ \\"very\\".unusual"@strange.example.com`,
+  // Allow all specials within quoted part
+  `"a?b(c)d,e:f;g<h>i[j\\k]l"@example.com`,
+  // Partially quoted is allowed, as long as separated by .
+  `just."weird but".fine@example.com`,
+];
+
+const validIpDomains = [
+  // IP addresses are allowed instead of domains when in square brackets, but strongly discouraged
+  `postmaster@[123.123.123.123]`,
+  // IPv6 uses a different syntax
+  `postmaster@[IPv6:2001:0db8:85a3:0000:0000:8a2e:0370:7334]`,
+  // IPv6 with shortened syntax
+  `postmaster@[IPv6:2001:db8:85a3::8a2e:370:7334]`,
+];
+
+const suites = [
+  [{ quotes: true, ips: true, unicode: true }, [validAddresses, validQuotedAddresses, validIpDomains, validUnicodeAddresses], [invalidAddresses]],
+  [{ quotes: true, ips: true, unicode: false }, [validAddresses, validQuotedAddresses, validIpDomains], [invalidAddresses, validUnicodeAddresses]],
+  [{ quotes: true, ips: false, unicode: true }, [validAddresses, validQuotedAddresses, validUnicodeAddresses], [invalidAddresses, validIpDomains]],
+  [{ quotes: true, ips: false, unicode: false }, [validAddresses, validQuotedAddresses], [invalidAddresses, validIpDomains, validUnicodeAddresses]],
+  [{ quotes: false, ips: true, unicode: true }, [validAddresses, validIpDomains, validUnicodeAddresses], [invalidAddresses, validQuotedAddresses]],
+  [{ quotes: false, ips: true, unicode: false }, [validAddresses, validIpDomains], [invalidAddresses, validQuotedAddresses, validUnicodeAddresses]],
+  [{ quotes: false, ips: false, unicode: true }, [validAddresses, validUnicodeAddresses], [invalidAddresses, validQuotedAddresses, validIpDomains]],
+  [{ quotes: false, ips: false, unicode: false }, [validAddresses], [invalidAddresses, validQuotedAddresses, validIpDomains, validUnicodeAddresses]],
+];
+
+for (const [config, valids, invalids] of suites) {
+  const emailRegex = createEmailRegex(config);
+  describe(`createEmailRegex(${JSON.stringify(config)})`, () => {
+    it("Should test true only for valid email", () => {  
+      for (const group of valids) {
+        for (const address of group) {
+          strictEqual(`${address}: ${emailRegex.test(address) ? "valid" : "invalid"}`, `${address}: valid`);
+        }
+      }
+    });
+    it("Should test false for invalid email", () => {
+      for (const group of invalids) {
+        for (const address of group) {
+          strictEqual(`${address}: ${emailRegex.test(address) ? "valid" : "invalid"}`, `${address}: invalid`);
+        }
+      }
+    });
+  });
+}
+
